538d1e3abcea536ee8c988db7287156192bd870c
Khant Sithu <khantseithuu@gmail.com>
2023-04-08 20:59:10 +0630

    Refactor into generic hook & remove duplication Fixes #12

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,43 +1,13 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+import useData from './useData';
 
-interface Genre{
-  id: number,
-  name: string,
+interface Genre {
+  id: number;
+  name: string;
 }
 
-interface FetchGenreResponse{
-  count: number,
-  results: Genre[]
-}
+const useGenres = () => useData<Genre>('/genres');
 
-const useGenres = () => {
-    const [genres, setGenres] = useState<Genre[]>([]);
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-      const controller = new AbortController();
-
-      setLoading(true);
-      apiClient
-        .get<FetchGenreResponse>('/genres', { signal: controller.signal })
-        .then((res) => {
-          setGenres(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) => {
-          if (err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        });
-
-      setLoading(false)
-      return () => controller.abort();
-    });
-
-    return { genres, error, loading };
-}
-
-export default useGenres
+export default useGenres;
