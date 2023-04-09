1441faec1bc1d817646f0f2f39c5ed8046c06b4a
Khant Sithu <khantseithuu@gmail.com>
2023-04-09 03:52:43 +0000

    updated api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: '96a4d70610f149b69ea913d7d520d28c'
+        key: '7365a3215e1d4af4bd27173969c80c41'
     }
 })
 
